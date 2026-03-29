0f6473c7476a0fc46989d0b4e5db62d0f7855149
Mark <m4rc0d3r@gmail.com>
2026-03-29 17:05:22 +0300

    chore: prevent ESLint from checking compiled files

--- apps/backend/eslint.config.ts
+++ apps/backend/eslint.config.ts
@@ -1,5 +1,8 @@
+import { globalIgnores } from "eslint/config";
+
 import { base } from "../../tooling/eslint/base-config";
 
 export default base({
   tsconfigRootDir: import.meta.dirname,
+  configs: [globalIgnores([".wrangler", "dist"])],
 });
